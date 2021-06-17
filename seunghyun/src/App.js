c5ffb1d3aceed0d5d2b116ad569dea73a712e63f
NocturneBliss <sean0820@snu.ac.kr>
2021-06-17 18:56:05 +0900

    지도 base 생성
    카카오 지도 api 사용하여 기본 지도 생성

--- seunghyun/src/App.js
+++ seunghyun/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from "react";
 import './App.css';
 
+const { kakao } = window;
+
+
+const options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3, //지도의 레벨(확대, 축소 정도)
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+        const options = {
+            center: new kakao.maps.LatLng(37.45657761349089, 126.95003849793633),
+            level: 5
+        };
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id='myMap' style={{
+    width: '100%',
+    height: '100vh'
+    }}/>
+    );
 }
 
 export default App;
