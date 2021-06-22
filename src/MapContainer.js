66eeae8c19b4dbe8ed17041e3aa96a47302358c6
bae-sojeong <82987238+bae-sojeong@users.noreply.github.com>
2021-06-22 17:53:00 +0900

    Add files via upload

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect }, {Component} from "react";
 import "./MapContainer.css";
 import {
   setJjim,
@@ -7,6 +7,7 @@ import {
   signInWithGoogle,
   auth,
 } from "./Firebase";
+import {Component} from "react";
 
 const { kakao } = window;
 
@@ -157,7 +158,7 @@ const MapContainer = ({
           : "취소"
         : "찜";
       jjimBtn.addEventListener("click", (e) => {
-        e.preventDefault();
+        e.peventDefault();
         !currentUser &&
           window.confirm("로그인 후 이용하시겠습니까?") &&
           signInWithGoogle() &&
@@ -178,31 +179,50 @@ const MapContainer = ({
       return el;
     }
 
+   /* addMaker >> component화
+    function addMarker (props) {
+      return(
+          <div position: {props.position}, idx: {props.idx}, title: {props.title}
+    const imageSrc =
+    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+    imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
+    imgOptions = {
+        spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+            spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+            offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+      },
+    markerImage = new kakao.maps.MarkerImage(
+    imageSrc,
+    imageSize,
+    imgOptions
+    )/>
+    )} */
+
     // 마커를 생성하고 지도 위에 마커를 표시하는 함수
     function addMarker(position, idx, title) {
       const imageSrc =
-          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
-        imgOptions = {
-          spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-          spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-          offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-        },
-        markerImage = new kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          imgOptions
-        ),
-        marker = new kakao.maps.Marker({
-          position: position, // 마커의 위치
-          image: markerImage,
-        });
+              "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+          imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
+          imgOptions = {
+            spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+            spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+            offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+          },
+          markerImage = new kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+              imgOptions
+          ),
+          marker = new kakao.maps.Marker({
+            position: position, // 마커의 위치
+            image: markerImage,
+          });
 
       marker.setMap(map); // 지도 위에 마커를 표출
       markers.push(marker); // 배열에 생성된 마커를 추가
 
       return marker;
-    }
+
 
     // 지도 위에 표시되고 있는 마커를 모두 제거
     function removeMarker() {
